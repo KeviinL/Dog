89bc16e11dfaf5dccec3b6ade315f56ab2e8c8cd
KeviinL <kevluna2002@gmail.com>
2022-02-07 13:51:16 -0600

    Made dog.js and dog.ts.
    Co-authored-by: lunarjuice <lunarjuice@users.noreply.github.com>
    Co-authored-by: kahaynes <kayhaynes@users.noreply.github.com>
    Co-authored-by: Armani1155 <Armani1155@users.noreply.github.com>

---
+++ dog.js
@@ -0,0 +1,36 @@
+var Dog = /** @class */ (function () {
+    function Dog(name, age, numOfLegs, hasFur, barks) {
+        this.name = name;
+        this.age = age;
+        this.numOfLegs = numOfLegs;
+        this.hasFur = hasFur;
+        this.barks = barks;
+    }
+    Dog.prototype.DogBark = function (doesDogBark) {
+        this.barks = doesDogBark;
+        if (doesDogBark == true) {
+            return "bark bark!";
+        }
+        return "the dog refused to bark";
+    };
+    Dog.prototype.DogName = function (dogsName) {
+        this.name = dogsName;
+        return "My dogs name is" + this.name;
+    };
+    Dog.prototype.DogAge = function (dogAge) {
+        this.age = dogAge;
+        return dogAge;
+    };
+    Dog.prototype.DogNumOfLegs = function (dogLegs) {
+        this.numOfLegs = dogLegs;
+        return dogLegs;
+    };
+    Dog.prototype.DogHasFur = function (doesDogHaveFur) {
+        this.hasFur = doesDogHaveFur;
+        if (doesDogHaveFur == true) {
+            return "My dog has fur";
+        }
+        return "My dog is bald!";
+    };
+    return Dog;
+}());
---
+++ dog.ts
@@ -0,0 +1,47 @@
+class Dog {
+    numOfLegs: number;
+    hasFur: boolean;
+    barks: boolean;
+    name: string;
+    age: number;
+
+    constructor(name: string, age: number, numOfLegs: number, hasFur: boolean, barks: boolean) {
+        this.name = name;
+        this.age = age;
+        this.numOfLegs = numOfLegs;
+        this.hasFur = hasFur;
+        this.barks = barks;
+    }
+
+    DogBark(doesDogBark: boolean): string {
+        this.barks = doesDogBark;
+        if(doesDogBark == true) {
+            return "bark bark!";
+        }
+        return "the dog refused to bark";
+    }
+
+    DogName(dogsName: string): string {
+        this.name = dogsName;
+        return "My dogs name is" + this.name;
+    }
+
+    DogAge(dogAge: number): number {
+        this.age = dogAge;
+        return dogAge;
+    }
+
+    DogNumOfLegs(dogLegs: number): number {
+        this.numOfLegs = dogLegs; 
+        return dogLegs;
+    }
+    
+    DogHasFur(doesDogHaveFur: boolean): string {
+        this.hasFur = doesDogHaveFur;
+        if(doesDogHaveFur == true) {
+            return "My dog has fur";
+        }
+        return "My dog is bald!";
+    }
+
+}
